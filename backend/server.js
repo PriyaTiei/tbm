a622cdeb90d18460ded009daf4498a5149ae97fc
MOHAMMED YOUSUF <yousuf@tiei.toyota-industries.com>
2023-05-19 12:28:02 +0530

    images removed form backen public folder

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 // const envPath = `${__dirname}/config/config.env`;
-// const envPath = `${__dirname}/config/config.env`;
+// dotenv.config({path:envPath});
 dotenv.config();
 
 const mongoPath = process.env.MONGO_PATH || "10.82.126.73";
-mongoose.connect(`mongodb://${mongoPath}:27017/autoMaint`);
+mongoose.connect(`mongodb://${mongoPath}:27017/${process.env.DB_NAME}`);
 
 process.on("uncaughtException", (err) => {
   console.log(`Error : ${err.message}`);
   console.log("closing process");
   process.exit(1);
 });
-
-
 
 const port = process.env.PORT || 5051;
 
