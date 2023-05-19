cd4e4747b9eea677191d26463b6f33070896e66e
MOHAMMED YOUSUF <yousuf@tiei.toyota-industries.com>
2023-05-19 14:02:14 +0530

    imported react

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { navBarSlice } from "../redux/navbarSlice";
 import { Button, Offcanvas } from "react-bootstrap";
