a869c2b28557dad65250061002fa1f0b1471d85d
yousuf <yousuf.rvce@gmail.com>
2024-11-22 09:19:24 +0530

    pending card alert if more than 30 days

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-smtp = 'smtp.gmail.com'
-username= 'username@gmail.com'
-password= 'password'
-from_email = 'from@gmail.com'
-production_to_email = 'prodto@gmail.com'
-server_url = 'http://localhost:5051'
-maintainance_to_email = 'maintto@gmail.com'
+smtp = "smtp.office365.com"
+username= "pankaj.jogi@tiei.toyota-industries.com"
+password= "WB4psCxK"
+from_email = "pankaj.jogi@tiei.toyota-industries.com"
+production_to_email =  "yousuf@tiei.toyota-industries.com,sundeep@tiei.toyota-industries.com, benjamindavid.s@tiei.toyota-industries.com"
+server_url = 'http://10.82.126.73:5071'
+maintainance_to_email =  "yousuf@tiei.toyota-industries.com,sundeep@tiei.toyota-industries.com, benjamindavid.s@tiei.toyota-industries.com"
